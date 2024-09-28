3146618d1580c059f282d4101179b87fcb622190
Filipi1503 <114451272+Filipi1503@users.noreply.github.com>
2024-09-27 22:12:31 -0300

    primeiro commit

---
+++ src/main/java/com/financybox/FinancyBox/FinancyBoxApplication.java
@@ -0,0 +1,13 @@
+package com.financybox.FinancyBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancyBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancyBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financybox/FinancyBox/FinancyBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financybox.FinancyBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancyBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
